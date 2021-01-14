f0aa2709b473545a1433f8771203def866317807
Tariq-star <77430165+Tariq-star@users.noreply.github.com>
2021-01-14 09:29:17 -0800

    First Upload

---
+++ WebDevExercise/Crash_Course_Starter_Code/app.js
@@ -0,0 +1,21 @@
+// Write our JavaScript in here
+var pics = [
+	"imgs/kitty_bed.JPG",
+	"imgs/kitty_basket.JPG", 
+	"imgs/kitty_laptop.jpg",
+	"imgs/kitty_door.jpg",
+	"imgs/kitty_sink.JPG",
+	"imgs/kitty_wall.jpg"
+];
+
+var btn = document.querySelector("button");
+var img = document.querySelector("img");
+var counter = 1;
+
+btn.addEventListener("click", function(){
+	if(counter === 6){
+		counter = 0;
+	}
+    img.src = pics[counter]
+	counter = counter + 1;
+});
